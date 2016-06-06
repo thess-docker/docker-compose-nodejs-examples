8ec8b4dd1b5fc7318eab6e01bb7ba98d008922dd
TheodorosPloumis <me@theodorosploumis.com>
2016-06-06 19:33:27 +0300

    Show more info on webpage

--- 05-nginx-express-redis-nodemon/app/routes/index.js
+++ 05-nginx-express-redis-nodemon/app/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var redis = require('redis');
+var os = require("os");
+var serverHostname = os.hostname();
 
 var router = express.Router();
 
@@ -8,6 +10,8 @@ var host = process.env.REDIS_PORT_6379_TCP_ADDR || '127.0.01';
 var port = process.env.REDIS_PORT_6379_TCP_PORT || 6379;
 var client = redis.createClient(port, host);
 
+var redisHostname = host;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   client.incr('counter', function(err, result) {
@@ -15,7 +19,13 @@ router.get('/', function(req, res, next) {
       return next(err);
     }
 
-    res.render('index', { title: 'Express', counter: result });
+    res.render('index', {
+      title: 'Express + Docker',
+      counter: result,
+      serverHostname: serverHostname,
+      redisHostname: redisHostname
+    });
+
   });
 });
 
